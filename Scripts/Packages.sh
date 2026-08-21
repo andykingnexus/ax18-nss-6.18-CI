ff8a3109c0758ea90ffa3589539f320b6f6d15e3
andykingnexus <63296999+andykingnexus@users.noreply.github.com>
2026-08-21 17:38:19 +0800

    revert: remove Honk integration

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -e
 
-if [ "${WRT_HONK:-false}" != "true" ]; then
-  echo "Honk injection skipped."
-  exit 0
-fi
-
-HONK_DIR="$GITHUB_WORKSPACE/wrt/package/honk"
-rm -rf "$HONK_DIR"
-git clone --depth=1 https://github.com/QiuSimons/luci-app-honk.git "$HONK_DIR"
-
-test -f "$HONK_DIR/honk/Makefile"
-test -f "$HONK_DIR/luci-app-honk/Makefile"
-test -d "$HONK_DIR/luci-app-honk/po/zh_Hans"
-
-echo "WRT_HONK_HASH=$(git -C "$HONK_DIR" rev-parse --short HEAD)" >> "$GITHUB_ENV"
-echo "Honk source: $(git -C "$HONK_DIR" log -1 --format='%h %s')"
+echo "Skip third-party package injection."
